31f099644fd1a7d6ff47fa3e2977755071a0ca7a
ddraper <david.draper@alfresco.com>
2016-12-22 20:33:29 +0000

    Improved testing time (needs abstracting for reuse)

--- test/unit/specs/Sample.spec.js
+++ test/unit/specs/Sample.spec.js
@@ -18,15 +18,45 @@ server.respondWith('GET',
 
 // Describe the tests...
 describe('List.vue', () => {
-  it('should render correct contents', (done) => {
-    const vm = new Vue({
-      el: document.createElement('div'),
-      render: (h) => h(List),
-    });
-    setTimeout(function() {
-        expect(vm.$el.querySelector('li.components-lists-ListView__item').textContent).to.equal('Data Dictionary');
-        done();
-    }, 1500);
+   it('should render correct contents', (done) => {
+      const vm = new Vue({
+         el: document.createElement('div'),
+         render: (h) => h(List),
+       });
+
+      var timeout = 2000;
+      var selector = 'li.components-lists-ListView__item';
+      var p = new Promise(function(resolve, reject) {
+
+         var intervalId;
+         var timeoutId = setTimeout(() => {
+            clearInterval(intervalId);
+            reject('Could not find element');
+         }, timeout);
+         intervalId = setInterval(() => {
+            let result = vm.$el.querySelector(selector);
+            if (result)
+            {
+               clearInterval(intervalId);
+               clearTimeout(timeoutId);
+               resolve(result);
+            }
+         }, 100);
+         
+      });
+      
+      p.then((result) => {
+         expect(result.textContent).to.equal('Data Dictionary', 'Wrong text');
+         done();
+      }).catch((error) => {
+         done(error);
+      });
+
+
+    // setTimeout(function() {
+    //     expect(vm.$el.querySelector('li.components-lists-ListView__item').textContent).to.equal('Data Dictionary');
+    //     done();
+    // }, 1500);
     
   });
 });
